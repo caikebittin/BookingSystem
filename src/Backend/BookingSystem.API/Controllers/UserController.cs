6cb745e3f0e22c1deb41098330f9cc785c59e403
Caike Bittin <ikee.bittin@gmail.com>
2025-08-03 19:49:49 -0300

    feat: adding endpoint to register an user.

---
+++ src/Backend/BookingSystem.API/Controllers/UserController.cs
@@ -0,0 +1,17 @@
+﻿using BookingSystem.Communication.Requests;
+using BookingSystem.Communication.Responses;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookingSystem.API.Controllers;
+[Route("[controller]")]
+[ApiController]
+public class UserController : ControllerBase
+{
+    [HttpPost]
+    [ProducesResponseType(typeof(ResponseRegisteredUserJson), StatusCodes.Status201Created)]
+    public IActionResult Register(RequestRegisterUserJson request)
+    {
+        return Created();
+    }
+}
